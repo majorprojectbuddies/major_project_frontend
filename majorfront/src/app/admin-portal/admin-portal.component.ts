373948866b50d094f3b1085397022215ecb3170e
Sameer Madan <sameermadan7@gmail.com>
2018-12-08 07:36:51 +0530

    removed logout btn from admin

--- majorfront/src/app/admin-portal/admin-portal.component.ts
+++ majorfront/src/app/admin-portal/admin-portal.component.ts
@@ -64,14 +64,7 @@ export class AdminPortalComponent implements OnInit {
     });
   }
 
-  onlogOutClick(){
-    this.timetableMessage = "default message";
-    this.timetabledropdownMessage = "default message";
-    this.adminMessage = "default message";
-    this.adminprofileMessage = "default message";
-    this.router.navigate(['']);
-  }
-
+  
   UpdateTimeTableClick(val : string){
     console.log("faculty is" + val);
     this.adminprofileService.changeMessage(val);
